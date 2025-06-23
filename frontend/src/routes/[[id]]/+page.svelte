3cc93108912e81e3ba5301f280d6c4ee5ab19965
Sonny Alves Dias <10231464+SonnyAD@users.noreply.github.com>
2025-06-23 12:35:21 +0800

    Language selector

--- frontend/src/routes/[[id]]/+page.svelte
+++ frontend/src/routes/[[id]]/+page.svelte
@@ -55,7 +55,7 @@
 	import EmojiBurst from '$lib/components/EmojiBurst.svelte';
 	import InputFlex from '$lib/components/InputFlex.svelte';
 	import { m } from '$lib/paraglide/messages.js';
-	import { setLocale } from '$lib/paraglide/runtime';
+	import { getLocale, setLocale } from '$lib/paraglide/runtime';
 
 	const opinions = {
 		stronglyAgree: "Complètement d'accord",
@@ -401,7 +401,6 @@
 	}
 
 	onMount(() => {
-		setLocale('en');
 		connectToPeer();
 		currentOpinion = 'notReplied';
 
@@ -1132,6 +1131,57 @@
 		offsetSubtitle={OFFSET_SUBSTITLE}
 		title={HEADER_TITLE}
 	/>
+
+	<div class="dropdown dropdown-end float-right">
+		<div
+			tabindex="0"
+			role="button"
+			class="btn btn-sm btn-ghost gap-1 px-1.5 text-[.5625rem] font-bold"
+			aria-label="Language"
+			title="Change Language"
+		>
+			<svg
+				class="text-base-content/70 size-4"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linejoin="round"
+					stroke-linecap="round"
+					stroke-width="2"
+					fill="none"
+					stroke="currentColor"
+					d="M12 21a9 9 0 1 0 0-18m0 18a9 9 0 1 1 0-18m0 18c2.761 0 3.941-5.163 3.941-9S14.761 3 12 3m0 18c-2.761 0-3.941-5.163-3.941-9S9.239 3 12 3M3.5 9h17m-17 6h17"
+				></path></svg
+			>
+			<svg
+				class="mt-px hidden size-2 fill-current opacity-60 sm:inline-block"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 2048 2048"
+				><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg
+			>
+		</div>
+		<div
+			tabindex="0"
+			class="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-8 h-[15rem] max-h-[calc(100vh-8.6rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline-1 outline-black/5"
+		>
+			<ul class="menu menu-sm w-full">
+				<li>
+					<button class:menu-active={getLocale() === 'en'} onclick={() => setLocale('en')}
+						><span class="pe-4 font-mono text-[.5625rem] font-bold tracking-[0.09375rem] opacity-40"
+							>EN</span
+						> <span class="font-[sans-serif]">English</span></button
+					>
+				</li>
+				<li>
+					<button class:menu-active={getLocale() === 'fr'} onclick={() => setLocale('fr')}
+						><span class="pe-4 font-mono text-[.5625rem] font-bold tracking-[0.09375rem] opacity-40"
+							>FR</span
+						> <span class="font-[sans-serif]">Français</span></button
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
 
 	<div class="m-4 mt-8 flex flex-wrap items-center justify-center gap-4 font-mono">
 		<span class="px-4 py-2">
