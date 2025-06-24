b32c1023c67b3e3c85d7ff02f0dd4a1486479784
Sonny Alves Dias <10231464+SonnyAD@users.noreply.github.com>
2025-06-24 23:21:38 +0800

    Updates logo

--- frontend/src/lib/env.js
+++ frontend/src/lib/env.js
@@ -10,7 +10,7 @@ export const PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL ?? 'https://spectrum.o
 export const LOGO_URL = import.meta.env.VITE_LOGO_URL ?? `/logo-white.png`;
 export const LOGO_WIDTH = import.meta.env.VITE_LOGO_WIDTH ?? 128;
 export const HEADER_TITLE = import.meta.env.VITE_HEADER_TITLE;
-export const OFFSET_SUBSTITLE = import.meta.env.VITE_OFFSET_SUBTITLE ?? -32;
+export const OFFSET_SUBSTITLE = import.meta.env.VITE_OFFSET_SUBTITLE ?? 0;
 
 // Feature flag
 export const ENABLE_AUDIO = import.meta.env.VITE_ENABLE_AUDIO ?? false;
