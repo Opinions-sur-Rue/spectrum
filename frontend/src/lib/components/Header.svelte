a2c4a1e9122306716470d1023731d5a54d91a7fe
Sonny Alves Dias <10231464+SonnyAD@users.noreply.github.com>
2025-06-27 10:42:53 +0800

    Improvements on navbar with more info

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -3,29 +3,30 @@
 	export let subtitle: string | undefined = undefined;
 	export let logo: string | undefined = undefined;
 	export let logoWidth: number = 64;
-	export let offsetSubtitle: number = -10;
 </script>
 
-<h1 class="font-mono text-4xl">
-	{#if logo}<img
-			src={logo}
-			alt="Spectrum"
-			width={logoWidth}
-			style="display: inline;"
-		/>&nbsp;{/if}{title}
-</h1>
+<div class="header flex">
+	<h1 class="flex-none font-mono text-4xl">
+		{#if logo}<img src={logo} alt="Spectrum" width={logoWidth} style="display: inline;" />{/if}<span
+			class="ml-1">{title}</span
+		>
+	</h1>
 
-{#if subtitle}
-	{#if logo && title}
-		<p class="font-thin italic" style="margin-left: 92px; margin-top: {offsetSubtitle}px;">
-			{subtitle}
-		</p>
-	{:else if logo}
-		<p class="font-thin italic" style="margin-left: 10px; margin-top: {offsetSubtitle}px;">
-			{subtitle}
-		</p>
-	{/if}
-{/if}
+	<div class="flex-1 p-4">
+		{#if subtitle}
+			{#if logo && title}
+				<p class="mt-2 text-center font-thin italic">
+					{subtitle}
+				</p>
+			{:else if logo}
+				<p class="mt-2 ml-6 text-sm font-thin italic">
+					{subtitle}
+				</p>
+			{/if}
+		{/if}
+		<slot />
+	</div>
+</div>
 
 <style>
 </style>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,6 +6,13 @@
 	import { NotificationDisplay } from '$lib/notifications';
 	import { getLocale, setLocale } from '$lib/paraglide/runtime';
 	import { m } from '$lib/paraglide/messages.js';
+	import Fa from 'svelte-fa';
+	import {
+		faCircleInfo,
+		faFileCircleQuestion,
+		faGlobe,
+		faSignal
+	} from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <svelte:head>
@@ -14,39 +21,106 @@
 </svelte:head>
 
 <section>
-	<div class="dropdown dropdown-end float-right mt-4 mr-4 md:mt-0 md:mr-8">
-		<div
-			tabindex="0"
-			role="button"
-			class="btn btn-sm btn-ghost gap-1 px-1.5 text-[.5625rem] font-bold"
-			aria-label="Language"
-			title="Change Language"
-		>
-			<svg
-				class="text-base-content/70 size-4"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				><path
-					stroke-linejoin="round"
-					stroke-linecap="round"
-					stroke-width="2"
-					fill="none"
-					stroke="currentColor"
-					d="M12 21a9 9 0 1 0 0-18m0 18a9 9 0 1 1 0-18m0 18c2.761 0 3.941-5.163 3.941-9S14.761 3 12 3m0 18c-2.761 0-3.941-5.163-3.941-9S9.239 3 12 3M3.5 9h17m-17 6h17"
-				></path></svg
+	<ul class="menu menu-horizontal bg-base-100 rounded-box float-right mt-6">
+		<li class="dropdown dropdown-end tooltip hidden" data-tip="Signal">
+			<div
+				tabindex="0"
+				role="button"
+				class="btn btn-ghost rounded-field indicator after:ml-2 after:transition-transform after:duration-200 after:content-['+']"
 			>
-			<svg
-				class="mt-px hidden size-2 fill-current opacity-60 sm:inline-block"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 2048 2048"
-				><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg
+				<Fa icon={faSignal} class="text-lg " />
+			</div>
+			<div tabindex="0" class="dropdown-content bg-base-100 rounded-box z-1 w-max shadow-sm">
+				<div class="stats">
+					<div class="stat">
+						<div class="stat-title">Server Ping</div>
+						<div class="stat-value">120ms</div>
+					</div>
+
+					<div class="stat">
+						<div class="stat-title">Server Status</div>
+						<div class="stat-value text-success">Healthy</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="dropdown dropdown-end tooltip" data-tip="Feedback">
+			<div tabindex="0" role="button" class="btn btn-ghost rounded-field">
+				<Fa icon={faFileCircleQuestion} class="text-lg" />
+			</div>
+			<div
+				tabindex="0"
+				class="card card-sm dropdown-content bg-base-100 rounded-box z-1 w-max shadow-sm"
 			>
-		</div>
-		<div
-			tabindex="0"
-			class="dropdown-content bg-base-200 text-base-content rounded-box top-px mt-8 h-[15rem] max-h-[calc(100vh-8.6rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline-1 outline-black/5"
-		>
-			<ul class="menu menu-sm w-full">
+				<div tabindex="0" class="card-body">
+					<h2 class="card-title">Vous avez un peu de temps?</h2>
+					<p>
+						Aidez-nous à améliorer la plateforme en faisant vos retours sur le formulaire suivant : <a
+							class="link link-info"
+							target="_blank"
+							rel="noopener"
+							href="https://docs.google.com/forms/d/e/1FAIpQLSd53Jkrxa0l1Vcp4rTtrRlSJ8d7Vxx42EiMz0esjAvVSmvLNg/viewform?usp=header"
+							>Google Form</a
+						>
+					</p>
+				</div>
+			</div>
+		</li>
+		<li class="dropdown dropdown-end tooltip hidden md:block" data-tip="Informations">
+			<div tabindex="0" role="button" class="btn btn-ghost rounded-field">
+				<Fa icon={faCircleInfo} class="text-lg" />
+			</div>
+			<ul class="list dropdown-content bg-base-100 rounded-box z-1 w-3xl max-w-screen shadow-sm">
+				<li class="list-row">
+					<div>
+						<div class="font-bold">C'est quoi un Spectrum ?</div>
+						<div class="badge badge-info text-xs">Définition</div>
+					</div>
+					<p class="list-col-wrap text-xs">
+						Spectrum est un outil visuel et interactif conçu pour faciliter les discussions
+						constructives entre plusieurs personnes autour de croyances ou d'opinions. <br />
+						Inspiré de la méthode de l'entretien épistémique (EE), il permet à chacun de positionner
+						son niveau de confiance sur une échelle allant du doute au fort assentiment, tout en expliquant
+						les raisons qui motivent cette position. <br />
+						L'objectif n'est pas de débattre pour convaincre, mais d'encourager la réflexion critique,
+						la clarification des idées et la compréhension mutuelle. En rendant visibles les divergences
+						et les convergences, le Spectrum favorise un dialogue respectueux et introspectif, que ce
+						soit en présentiel ou en ligne.
+					</p>
+				</li>
+
+				<li class="list-row">
+					<div>
+						<div class="font-bold">Qu'est-ce qu'un Homme de fer ?</div>
+						<div class="badge badge-info text-xs">Définition</div>
+					</div>
+					<p class="list-col-wrap text-xs">
+						Un homme de fer est une technique de débat qui consiste à reformuler l'argument de son
+						interlocuteur de la manière la plus forte, claire et juste possible, parfois même mieux
+						que lui ne l'aurait exprimé lui-même. <br />
+						Cela permet de montrer qu'on a bien compris sa position et d'engager un dialogue respectueux
+						et constructif, loin des caricatures ou des attaques simplistes. C'est pourquoi il est couramment
+						utilisé lors de Spectrum. <br />
+						C'est l'opposé de l'homme de paille, qui déforme un argument pour mieux le réfuter. L'homme
+						de fer vise au contraire la compréhension mutuelle.
+					</p>
+				</li>
+			</ul>
+		</li>
+		<li class="dropdown dropdown-end tooltip" data-tip="Langues">
+			<div tabindex="0" role="button" class="btn btn-ghost rounded-field">
+				<Fa icon={faGlobe} class="text-lg" />
+				<svg
+					width="12px"
+					height="12px"
+					class="inline-block h-2 w-2 fill-current opacity-60"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 2048 2048"
+				>
+					<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+				</svg>
+			</div>
+			<ul class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
 				<li>
 					<button class:menu-active={getLocale() === 'en'} onclick={() => setLocale('en')}
 						><span class="pe-4 font-mono text-[.5625rem] font-bold tracking-[0.09375rem] opacity-40"
@@ -62,8 +136,8 @@
 					>
 				</li>
 			</ul>
-		</div>
-	</div>
+		</li>
+	</ul>
 
 	<article class="p-2 md:m-4 lg:m-8">
 		<slot />
